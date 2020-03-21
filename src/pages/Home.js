9151493a260793d3f20b0c918283f867b4ccfbc2
David Bronne <davidbronne@gmail.com>
2020-03-21 19:22:27 +0100

    Style Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,8 +2,12 @@ import React from 'react'
 
 function Home() {
   return (
-    <div> 
-      <h1>Home Page</h1>
+    <div className="home"> 
+      <h1 className="title is-1">Hacka Culture</h1>
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTvaAtDfOcnxTLC0dLd8DBE_k0Vw7CaUPv9428FZZE2Sj0J_Zyt" alt="homeLogo"></img>
+      <p className="subtitle is-3">Hack Yourself</p>
+      <p className="subtitle is-3">Support NGO</p>
+      <p className="subtitle is-3">Build Network</p>
     </div>
   )
 }
