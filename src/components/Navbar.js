be7d7d134f838814b5b3000f6f729ff5067350f1
David Bronne <davidbronne@gmail.com>
2020-03-21 17:32:04 +0100

    Create Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,46 +8,50 @@ class Navbar extends Component {
 
     return (
       <div>
-        <nav className="navbar">
-          
-          {isLoggedIn 
-          
+          {isLoggedIn   
           ? (
-            <>
-              <Link className="navbar-item" to={"/user-profile-edit"}>
-                <h4 className="navbar-button">{user.firstName}</h4>
-              </Link>
-              
-              <Link className="navbar-item" to={"/"} id="home-btn">
-                <h4>
-                
-                <img src="https://pngriver.com/wp-content/uploads/2018/04/Download-Peace-Symbol-PNG-Image.png" alt="Home"></img>
-                </h4>
-              </Link>
+               <>
+                   <nav className="navbar">
+                      <div className="navbar-menu is-active">
+                          <Link className="navbar-item" to={"/user-profile-edit"}>
+                              <h4 className="navbar-button">{user.firstName}</h4>
+                          </Link>
+                      
+                          <Link className="navbar-item" to={"/"} id="home-btn">
+                              <h4>
+                                  <img src="https://pngriver.com/wp-content/uploads/2018/04/Download-Peace-Symbol-PNG-Image.png" alt="Home"></img>
+                              </h4>
+                         </Link>
 
-              <button className="navbar-button" onClick={logout}>Logout</button>
-            </>
-          ) 
-          
-          : (
-            <>
-              <Link className="navbar-item" to="/login">
-                {" "}
-                <button className="button is-rounded">Login</button>{" "}
-              </Link>
-              <br />
-              
-              <Link className="navbar-item" to={"/"} id="home-btn">
-              <img src="https://pngriver.com/wp-content/uploads/2018/04/Download-Peace-Symbol-PNG-Image.png" alt="Home"></img>
-              </Link>
-              
-              <Link to="/signup">
-                {" "}
-                <button className="button is-rounded">Sign Up</button>{" "}
-              </Link>
-            </>
-          )}
-        </nav>
+                          {/* <button className="button is-rounded" onClick={logout}>Logout</button> */}
+                          <div className="navbar-item" onClick={logout}>Logout</div>
+                      </div>
+            
+                    </nav>
+                </>
+            ) 
+                  
+            : (
+                <>
+                    <nav className="navbar">
+                        <div className="navbar-menu is-active">
+                            <Link className="navbar-item" to="/login">
+                                <div >Login</div>
+                            </Link>
+                            <Link className="navbar-item" to={"/"} id="home-btn">
+                                {/* <img src="https://pngriver.com/wp-content/uploads/2018/04/Download-Peace-Symbol-PNG-Image.png" alt="Home"></img> */}
+                                <span className="icon is-large">
+                                <i class="fas fa-peace fa-3x"></i>
+                                </span>
+                            
+                            </Link>
+                            <Link className="navbar-item" to="/signup"> 
+                                <div>Sign Up</div>
+                            </Link>
+                        </div>
+                    </nav>
+                </>
+              )}
 
         </div>
     );
